bc1c1b7f4162910b8edd59d7a3efce61a8435ec4
Bianca Farias <bifarias202@gmail.com>
2026-01-07 01:17:27 +0000

    Finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersionResult = await database.query("SHOW server_version;");
+  const postgres_version = postgresVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::integer AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    openedConnectionsResult.rows[0].opened_connections;
+
   response.status(200).json({
-    chave: "Olá, mundo!",
+    updated_at: updatedAt,
+    dependences: {
+      database: {
+        postgres_version: postgres_version,
+        max_connections: maxConnections,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  //testar se retonar a versão correta
+  const postgresVersion = responseBody.dependences.database.postgres_version;
+  expect(postgresVersion).toEqual("16.11");
+
+  //testa se retonar o número de maxConnections
+  const maxConnections = responseBody.dependences.database.max_connections;
+  expect(Number.isInteger(maxConnections)).toBe(true);
+
+  // teste de número de usedConnections
+  const usedConnections = responseBody.dependences.database.opened_connections;
+  expect(usedConnections).toBe(1);
 });
